8ca87143d0565dd66985c791747dbcfc231296a1
Francesco <francesco.moretti2803@gmail.com>
2026-09-15 18:01:54 +0200

    pagina 404

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express=require('express');
+const path=require('path');
 
 const pool=require('./src/db');
 
@@ -19,7 +20,13 @@ app.use(specialisticheRoutes);
 const interventiRoutes=require('./src/routes/interventiRoutes');
 app.use(interventiRoutes);
 
+//serve i file statici della cartella public
 app.use(express.static('public'));
+
+//404
+app.use((req, res)=>{
+    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));//cosi rimane il nome del file non trovato nel browser
+});
 
 app.listen(PORT, ()=>{
     console.log(`Server in esecuzione sulla porta ${PORT}`);
